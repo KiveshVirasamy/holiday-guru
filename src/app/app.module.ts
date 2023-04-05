68bec218a1044455f5893baae46e1ca3fa35ce23
KiveshVirasamy <124234366+KiveshVirasamy@users.noreply.github.com>
2023-04-05 08:41:17 +0200

    feat: :sparkles: Added NG-Zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,15 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
 
 
 
@@ -23,9 +32,14 @@ import { AppComponent } from './app.component';
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
     provideFirestore(() => getFirestore()),
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
 
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
