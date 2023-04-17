00b94d26dfd9985a1911ebbccc642569e12aae0f
KiveshVirasamy <124234366+KiveshVirasamy@users.noreply.github.com>
2023-04-17 08:43:00 +0200

    feat: :sparkles: Added routing and error handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzResultModule } from 'ng-zorro-antd/result';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import * as fromTripsState from 'src/app/store/reducers/trips.reducer';
@@ -37,6 +38,7 @@ import { CalendarComponent } from './components/calendar/calendar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DeleteActivityComponent } from './components/delete-activity/delete-activity.component';
 import { DeleteTripComponent } from './components/delete-trip/delete-trip.component';
+import { ErrorpageComponent } from './components/errorpage/errorpage.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
@@ -67,6 +69,7 @@ registerLocaleData(en);
     DeleteTripComponent,
     DeleteActivityComponent,
     CalendarComponent,
+    ErrorpageComponent,
 
 
 
@@ -93,6 +96,7 @@ registerLocaleData(en);
     NzListModule,
     NzCollapseModule,
     NzCalendarModule,
+    NzResultModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
     StoreModule.forFeature(fromTripsState.tripsFeatureKey, fromTripsState.reducer),
